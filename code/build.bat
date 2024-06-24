6416d59d818c987c92da9c577e1c43659d57ebae
elmarsan <mselias97@gmail.com>
2024-06-24 22:52:39 +0200

    001

---
+++ code/build.bat
@@ -0,0 +1,14 @@
+::Usage: execute from root directory
+
+:: do not output each command of the script
+@echo off
+
+:: Require cl (Microsoft visual studio tool for compiling using command line)
+
+:: Push directory
+mkdir ..\build
+pushd ..\build
+pwd
+cl -Zi ..\code\win32_handmade.cpp user32.lib
+:: Come back to the above directory
+popd
---
+++ code/win32_handmade.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    MessageBoxA(0, "Handmade hero 001", "Handmade hero", MB_OK | MB_ICONINFORMATION);
+    return 0;
+}
